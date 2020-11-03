fbb8b6ae25d8e0c78b2e58ce48ffd190465bca5c
Igor <igor.rangel@ciadocredito.net.br>
2020-11-02 22:02:33 -0300

    commit

---
+++ src/app/1_introducao/introducao.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'idr_introducao',
+	templateUrl: 'introducao.component.html',
+	styleUrls: ['introducao.component.css']
+})
+export class IntroducaoComponent {
+}
