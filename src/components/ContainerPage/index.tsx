07ff82d530cd1607d9e571d9c47fa2dc6cf4490e
Igor Rangel <igordrangel@gmail.com>
2024-05-25 12:49:06 -0300

    removendo arquivos

--- src/components/ContainerPage/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react'
-
-export function ContainerPage({ children }: { children: ReactNode }) {
-  return (
-    <main className="flex justify-center box-border m-1 ml-0 dark:bg-black">
-      <div>{children}</div>
-    </main>
-  )
-}
